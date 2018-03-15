4f69c539c158b544c7897638d027022c8a8f4823
Abraham Porter <abeywpm@live.com>
2018-03-15 15:18:08 -0600

    Bar selector works

---
+++ js/examples.js
@@ -0,0 +1,72 @@
+$(function() {
+    function ratingEnable() {
+        $('#perdCabello').barrating('show', {
+            theme: 'bars-1to10'
+        });
+        $('#caspa').barrating('show', {
+            theme: 'bars-1to10'
+        });
+        $('#picazon').barrating('show', {
+            theme: 'bars-1to10'
+        });
+        $('#enrojecimiento').barrating('show', {
+            theme: 'bars-1to10'
+        });
+        $('#ronchas').barrating('show', {
+            theme: 'bars-1to10'
+        });
+        $('#postulas').barrating('show', {
+            theme: 'bars-1to10'
+        });
+        $('#perdApetito').barrating('show', {
+            theme: 'bars-1to10'
+        });
+        $('#dolorCabeza').barrating('show', {
+            theme: 'bars-1to10'
+        });
+        $('#hinchazon').barrating('show', {
+            theme: 'bars-1to10'
+        });
+        $('#pielSeca').barrating('show', {
+            theme: 'bars-1to10'
+        });
+        $('#manchasRojas').barrating('show', {
+            theme: 'bars-1to10'
+        });
+        $('#fatiga').barrating('show', {
+            theme: 'bars-1to10'
+        });
+        $('#fiebre').barrating('show', {
+            theme: 'bars-1to10'
+        });
+        $('#espinillas').barrating('show', {
+            theme: 'bars-1to10'
+        });
+        $('#costras').barrating('show', {
+            theme: 'bars-1to10'
+        });
+        $('#dolorArticular').barrating('show', {
+            theme: 'bars-1to10'
+        });
+        $('#dolorGarganta').barrating('show', {
+            theme: 'bars-1to10'
+        });
+        $('#pielHoyuelos').barrating('show', {
+            theme: 'bars-1to10'
+        });
+        $('#dolorHuesos').barrating('show', {
+            theme: 'bars-1to10'
+        });
+        $('#calambres').barrating('show', {
+            theme: 'bars-1to10'
+        });
+        $('#congNasal').barrating('show', {
+            theme: 'bars-1to10'
+        });
+        $('#erupciones').barrating('show', {
+            theme: 'bars-1to10'
+        });
+    }
+
+    ratingEnable();
+});
